df84ea06b890288c8a2868b44b26082ff9a54922
jordimall <jordi.mallafre@hotmail.com>
2023-11-05 11:20:59 +0100

    finalicación del modelo Videos

---
+++ TA22_patronMVC_ejercicio02/src/main/java/TA22_patronMVC_ejercicio02/TA22_patronMVC_ejercicio02/App.java
@@ -0,0 +1,13 @@
+package TA22_patronMVC_ejercicio02.TA22_patronMVC_ejercicio02;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
